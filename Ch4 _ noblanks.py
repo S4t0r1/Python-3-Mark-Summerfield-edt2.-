c6ed603775f5b610db7cae905a0587c72560ffca
S4t0r1 <33390640+S4t0r1@users.noreply.github.com>
2018-01-12 09:33:01 +0100

    Update Ch4 _ noblanks.py

--- Ch4 _ noblanks.py
+++ Ch4 _ noblanks.py
@@ -1,20 +1,34 @@
 import sys, os
 
 
+def main():
+    if len(sys.argv) == 1 or sys.argv[1] in {"-h", "--help"}:
+        print("usage: {0} file [file2 [..fileN]]".format(sys.argv[0]))
+        sys.exit()
+
+    for filename in sys.argv[1:]:
+        lines = read_data(filename)
+        if lines:
+            write_data(lines, os.path.splitext(filename)[0] + ".nb")
+
+
 def read_data(filename):
     lines = []
     fh = None
     try:
-        fh = open(filename, encoding="utf8")
+        fh =open(filename, encoding="utf8")
         for line in fh:
             if line.strip():
                 lines.append(line)
-    except (IOError, OSError) as err:         # can be replaced with EnvironmentError as both are its subtypes
-        print(err)
+    except EnvironmentError as err:
+        print("ERROR: {0} could not be opened!: {1}".format(
+                                             filename, err))
         return []
+    else:
+        print("Succesfully extracted data from {0}".format(filename))
     finally:
         if fh is not None:
-          fh.close()
+            fh.close()
     return lines
 
 
@@ -25,18 +39,12 @@ def write_data(lines, filename):
         for line in lines:
             fh.write(line)
     except EnvironmentError as err:
-        print(err)
+        print("ERROR: {0} could not be saved!: {1}".format(
+                                             filename, err))
+    else:
+        print("Succesfully saved data to {0}".format(filename))
     finally:
         if fh is not None:
             fh.close()
 
-
-if len(sys.argv) < 2:
-    print("usage: noblanks.py file1 [file2 [.. fileN]]")
-    sys.exit()
-
-
-for filename in sys.argv[1:]:
-    lines = read_data(filename)
-    if lines:
-        write_data(lines, os.path.splitext(filename)[0] + ".nb")
+main()
